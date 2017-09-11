a2911ee729ff008748277a4aecbf13d0c57dd47a
Himanshu Maheshwari <m.himanshu2311@gmail.com>
2017-09-11 17:45:33 +0530

    Relevant Tweets

--- libs/get_relevant_tweets.py
+++ libs/get_relevant_tweets.py
@@ -5,21 +5,27 @@ import re
 
 users = ["@sardesairajdeep","@BDUTT","@sagarikaghose","@vikramchandra","@asharma","@SachinKalbag","@madversity","@cricketwallah","@Kanchangupta","@rahulkanwal","@timesofindia","@ndtv","@IndiaToday","@IndianExpress","@the_hindu","@CNNnews18","@firstpost","@bsindia","@dna","@DeccanChronicle","@Oneindia","@FinancialXpress","@BreakingNews","@BBCBreaking","@cnnbrk","@WSJbreakingnews","@CBSNews"]
 
+def combination(s):
+    return [[s[j] for j in xrange(len(s)) if (i&(1<<j))] for i in xrange(1<<len(s))]
+
 # Returns a list of relevant tweets as list based on keyword
 def find_all(keywords):
     # Create a search pattern
     relevant_tweets = []
-    search_pattern = keywords[0]
-    for i in range(1, len(keywords)):
-        search_pattern += ('|' + keywords[i])
+    words = combination(keywords)
     for user in users:
         data = db.child("users").child(user).get()
         try:
-            for i, (key, value) in enumerate(data.val().iteritems()):
-                # In each tweets find keywords
-                if(re.search(search_pattern, value, flags=re.IGNORECASE)):
-                    relevant_tweets.append(value)
+            i = len(words)
+            while(True):
+                if(i == 1):
                     break
+                i -= 1
+                for j, (id, tweet) in enumerate(data.val().iteritems()):
+                    # In each tweets find keywords
+                    if all(keyword in tweet for keyword in words[i]):
+                        relevant_tweets.append(tweet)
+                        break
         except Exception, e:
             print str(user) + " not found"
     return relevant_tweets
