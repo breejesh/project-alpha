a09e7f63f9a073047d6a7405617a92a405fd54aa
Himanshu Maheshwari <m.himanshu2311@gmail.com>
2017-09-11 17:03:00 +0530

    relevant tweets

--- libs/get_relevant_tweets.py
+++ libs/get_relevant_tweets.py
@@ -13,18 +13,13 @@ def find_all(keywords):
     for i in range(1, len(keywords)):
         search_pattern += ('|' + keywords[i])
     for user in users:
-        print user
         data = db.child("users").child(user).get()
         try:
             for i, (key, value) in enumerate(data.val().iteritems()):
                 # In each tweets find keywords
                 if(re.search(search_pattern, value, flags=re.IGNORECASE)):
                     relevant_tweets.append(value)
-<<<<<<< HEAD
                     break
-=======
-                    print value
->>>>>>> cf550a17829deaef848d1f11852ecb07381254cf
         except Exception, e:
             print str(user) + " not found"
     return relevant_tweets
