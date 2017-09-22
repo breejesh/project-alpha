47841950df6b23e7465a5cf240d9034420e461f2
Himanshu Maheshwari <m.himanshu2311@gmail.com>
2017-09-22 13:19:39 +0530

    Relevant Tweet Local Created

--- libs/get_relevant_tweets_local.py
+++ libs/get_relevant_tweets_local.py
@@ -1,12 +1,31 @@
 import tweepy
 import pyrebase
 from connection import db, api
+import json
 import re
 
 users = ["@timesofindia","@ndtv","@IndiaToday","@CNNnews18","@bsindia","@dna","@BreakingNews","@BBCBreaking"]
 
 def combination(s):
     return [[s[j] for j in xrange(len(s)) if (i&(1<<j))] for i in xrange(1<<len(s))]
+
+def load_data():
+    tweet = {}
+    try:
+        for user in users:
+            print user
+            try:
+                json_data = open('../data/' + user.lstrip('@') + '.json').read()
+            except Exception, e:
+                json_data = open('data/' + user.lstrip('@') + '.json').read()
+            data = json.loads(json_data)
+            for i in range(len(data)):
+                tweet[data[i]['id']] = data[i]['text']
+    except Exception, e:
+        print e
+        tweet = {}
+        return tweet            
+    return tweet
 
 # Returns a list of relevant tweets as list based on keyword
 def find_all(keywords):
@@ -16,7 +35,7 @@ def find_all(keywords):
     for user in users:
         print user
         flag = 0
-        data = db.child("users").child(user).get()
+        data = load_data()
         try:
             i = len(words)
             while(True):
@@ -36,3 +55,6 @@ def find_all(keywords):
         except Exception, e:
             print str(user) + " not found"
     return relevant_tweets
+
+if __name__ == '__main__':
+    load_data()
